b07cefa3c1af2ded6e583b8480f58b9389e306e6
ishsshukla <shukla22164024@akgec.ac.in>
2023-10-05 23:54:25 +0530

    project done

--- js/script.js
+++ js/script.js
@@ -1,96 +1,132 @@
+// Initialize variables
+let [sec, min, hrs] = [0, 0, 0];
+let startTime = 0;
+let lapTimes = [];
+let lapCounter = 1;
 
-      
+// Get elements
+let displayTime = document.getElementById("displayTime");
+let lapButton = document.getElementById("lapbutton");
+let startButton = document.getElementById("startButton");
 
-let[sec,min,hrs]=[0,0,0];                                           // declaring time 00:00:00
-let displayTime=document.getElementById("displayTime");            // to display time on webpage
-let timer=null;                                                   //  updates stopwatch display
+// Variable to track the stopwatch state
+let stopwatchRunning = false;
 
-function stopwatchtime(){
-                        // time showing hrs min and sec
-      sec++;
-      if(sec==60){
-            sec=0;
-            min++;
-        if(min==60){
-            min=0;
+// Function to display time
+function displayCurrentTime() {
+    let h = hrs < 10 ? "0" + hrs : hrs;
+    let m = min < 10 ? "0" + min : min;
+    let s = sec < 10 ? "0" + sec : sec;
+
+    displayTime.innerHTML = h + ":" + m + ":" + s;
+}
+
+// Function to start the stopwatch
+function startStopwatch() {
+    if (!stopwatchRunning) {
+        stopwatchRunning = true;
+        startButton.innerHTML = "Stop";
+        startButton.style.background = "red";
+        lapButton.disabled = false;
+        if (startTime === 0) {
+            startTime = Date.now() - (sec * 1000 + min * 60000 + hrs * 3600000);
+        }
+        timer = setInterval(stopwatchTime, 1000);
+    } else {
+        stopStopwatch();
+    }
+}
+
+// Function to stop the stopwatch
+function stopStopwatch() {
+    if (stopwatchRunning) {
+        stopwatchRunning = false;
+        clearInterval(timer);
+        startButton.innerHTML = "Resume";
+        startButton.style.background = "green";
+    }
+}
+
+// Function to handle lap button click
+function handleLapClick() {
+    if (stopwatchRunning) {
+        const currentTime = Date.now();
+        const lapTime = currentTime - startTime;
+
+        // Format the lap time
+        const lapHrs = Math.floor(lapTime / 3600000);
+        const lapMin = Math.floor((lapTime % 3600000) / 60000);
+        const lapSec = Math.floor((lapTime % 60000) / 1000);
+
+        const lapTimeString = `${lapHrs}:${lapMin}:${lapSec}`;
+
+        // Store lap time and lap number
+        lapTimes.push({ lap: lapCounter++, time: lapTimeString });
+
+        // Display lap time
+        const lapList = document.getElementById("lapList");
+        const lapItem = document.createElement("li");
+        lapItem.textContent = `Lap ${lapTimes.length}: ${lapTimeString}`;
+        lapList.appendChild(lapItem);
+
+        // Update start time for the next lap
+        startTime = currentTime;
+    }
+}
+
+// Function to reset the stopwatch
+function resetStopwatch() {
+    stopwatchRunning = false;
+    clearInterval(timer);
+    [sec, min, hrs] = [0, 0, 0];
+    displayCurrentTime();
+    lapButton.disabled = true;
+    startButton.innerHTML = "Start";
+    startButton.style.background = "green";
+    lapTimes = [];
+    lapCounter = 1;
+    const lapList = document.getElementById("lapList");
+    lapList.innerHTML = "";
+}
+
+// Event listeners
+startButton.addEventListener("click", startStopwatch);
+lapButton.addEventListener("click", handleLapClick);
+document.getElementById("resetButton").addEventListener("click", resetStopwatch);
+
+// Function to update stopwatch time
+function stopwatchTime() {
+    sec++;
+    if (sec == 60) {
+        sec = 0;
+        min++;
+        if (min == 60) {
+            min = 0;
             hrs++;
         }
+    }
+    displayCurrentTime();
+}
+
+// Initial display
+displayCurrentTime();
+
+displayCurrentTime();
+function handleKeyPress(event) {
+      if (event.ctrlKey) {
+          if (event.key === 's') {
+              startStopwatch();
+          } else if (event.key === 'x') {
+              stop();
+          } 
+          else if (event.key === 'r') {
+            resetStopwatch();
+        } else if (event.key === 'l') {
+          handleLapClick();
+      } else if (event.key === 'p') {
+        resume();
+    } 
       }
-      let h=hrs < 10 ? "0" + hrs : hrs;
-      let m=min < 10 ? "0" + min : min;
-      let s=sec < 10 ? "0" + sec : sec;
-
-      displayTime.innerHTML  = h + ":" + m + ":" + s;
-
-}
-
-function watchStart(){
-      if(timer!== null){
-            {
-                  clearInterval(timer);
-            }
-      }
-       // display time in every 1000 millisec
-      timer=setInterval(stopwatchtime,1000);
-
-      if(button_state == "start"){
-            //  If it's start, it changes  to Stop
-            start_button.innerHTML = "Stop";
-            start_button.style.background = "red";
-            button_state = "stop";
-      }
-      else{
-
-            // stopwatch is already running, so it stops & changes  to Resume
-            watchStop();
-            start_button.innerHTML = "Resume";
-            button_state = "start";
-            start_button.style.background = "green";
-            reset_button.innerHTML = "Reset" ;
-            // button_state = "lap";
-            // start_button.innerHTML = "Start";
-            // button_state = "Resume";
-            // start_button.style.background = "green";
-
-             }
-
-}
-
-function watchStop(){
-      if(timer!== null){
-            {
-                  clearInterval(timer);
-            }
-      }
-      // display time in every 1000 millisec
-      // timer=setInterval(stopwatchtime,1000);
-}
-
-// function resumeButton(){
-     
-//       // display time in every 1000 millisec
-//       timer=setInterval(stopwatchtime,1000);
-// }
-// function watchStop(){
-//       clearInterval(timer);
-// }
-
-function watchreset(){
-      clearInterval(timer);
-      [sec,min,hrs]=[0,0,0];
-      displayTime.innerHTML = "00:00:00";
-
-}
-function lap(){
-
-}
-            // event listeners 
-let start_button = document.getElementById("startButton");
-let Resume_button = document.getElementById("startButton");
-let reset_button = document.getElementById("lapbutton");
-var button_state = "start";
-reset_button.addEventListener("click",lap);
-start_button.addEventListener("click",watchStart);
-reset_button.addEventListener("click",watchreset);
-Resume_button.addEventListener("click",);
-// start_button.addEventListener("click",watchtart);
+  }
+  
+  document.addEventListener('keydown', handleKeyPress);
